944b02092c476daa5aa381cccc94fe18941716f0
G4br1elM4t0s <gabrielmatos010203@gmail.com>
2023-06-18 06:34:03 -0300

    refactor: new refactor

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: "postgres://devmovies:AEokZcgoReSWQ9FpZVTizMp6861YaAiN@dpg-ci752amnqql0ld9d0qq0-a.oregon-postgres.render.com/devmovies",
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
